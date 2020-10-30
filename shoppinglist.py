befc2579ce7036da464926ae5d0589b6db1681b4
nullresponse <69951149+nullresponse@users.noreply.github.com>
2020-10-30 16:49:23 +0800

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,11 +1,11 @@
-Shopping=True
-list=['youtube']
-while Shopping=True:
-    toadd=input('What do you want to add to the shopping list?')
-    list.append(toadd)
-    print('you have', list, 'in your shopping list')
-    shoppingstill=input('Do you still want to add to the list?')
-    if shoppingstill.lower='yes':
-        print('ok')
-        Shopping=False
+def mainMenu():
+    while True:
+        print()
+        print("SHOPPING LIST")
+        print("1. View shopping list")
+        print("2. Add item to shopping list")
+        print("3. Remove item from shopping list")
+        print("4. Check if item is on shopping list")
+        print("5. How many items on shopping list")
+        print("6. Clear shopping list")
     
